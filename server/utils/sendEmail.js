9a82303dda4952891891dd4feef3d08ae5889d6a
Anit Kaushik <anitkaushik.dev@gmail.com>
2026-06-28 15:03:08 +0530

    Fix nodemailer IPv6 issue

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,32 +1,33 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-const sendEmail = async (to, subject, text) => { 
-    
-    try {
+const sendEmail = async (to, subject, text) => {
+  try {
     const transporter = nodemailer.createTransport({
-      service: "gmail", 
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
+        pass: process.env.EMAIL_PASS,
+      },
     });
 
     await transporter.sendMail({
-      to, 
+      to,
       subject,
-      text 
+      text,
     });
 
     console.log("Email sent successfully");
   } catch (error) {
-     console.error("Email error:", error.message);
-  throw error;
+    console.error("Email error:", error.message);
+    throw error;
   }
 };
-
-
 
 export default sendEmail;
