d066f9d8a7584e1b474bc45620d86ba48aa5a341
Anit Kaushik <anitkaushik.dev@gmail.com>
2026-06-29 10:29:55 +0530

    mail SMTP  forgot pasword

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,14 +6,15 @@ dotenv.config();
 const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.verify();
     console.log("SMTP connection successful");
