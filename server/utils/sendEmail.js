08c8bd258ba1c4f12db528001198a246bc891b91
Anit Kaushik <anitkaushik.dev@gmail.com>
2026-06-30 15:31:02 +0530

    switch to brevo email api

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,40 +1,37 @@
-import dns from "dns";
-dns.setDefaultResultOrder("ipv4first");
-
-import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const sendEmail = async (to, subject, text) => {
   try {
-    
-    
-    const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.BREVO_EMAIL,
-        pass: process.env.BREVO_SMTP_KEY,
+    const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "POST",
+      headers: {
+        "accept": "application/json",
+        "api-key": process.env.BREVO_API_KEY,
+        "content-type": "application/json",
       },
+      body: JSON.stringify({
+        sender: {
+          name: "NovaShelf",
+          email: process.env.SENDER_EMAIL,
+        },
+        to: [
+          {
+            email: to,
+          },
+        ],
+        subject: subject,
+        textContent: text,
+      }),
     });
 
-    // await transporter.verify();
-    console.log("Brevo SMTP connected");
+    const data = await response.json();
 
-    const info = await transporter.sendMail({
-      from: `"NovaShelf" <${process.env.SENDER_EMAIL}>`,
-      to,
-      subject,
-      text,
-    });
+    console.log("Email API response:", data);
 
-    console.log("Email sent:", info.messageId);
-
-    return info;
+    return data;
   } catch (error) {
-    console.error(error);
+    console.error("Email API Error:", error);
     throw error;
   }
 };
